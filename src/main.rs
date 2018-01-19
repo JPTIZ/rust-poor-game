e7e640f5667abf7c5265806ce6a4601d3171d1c6
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2018-01-19 16:22:28 -0200

    simple sfml window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate sfml;
+
+use sfml::graphics::RenderWindow;
+
+use sfml::window::{
+    ContextSettings,
+    Style,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let context_settings = ContextSettings{
+        antialiasing_level: 0,
+        ..Default::default()
+    };
+
+    let mut window = RenderWindow::new(
+        (640, 480),
+        "RPG",
+        Style::CLOSE,
+        &context_settings
+        );
+
+    while window.is_open() {
+        window.display();
+    }
 }
